4d747bb6583bb0bbc38f3105502351c638869bf6
Chai Feng <chaifeng@chaifeng.com>
2019-07-31 15:50:41 +0200

    Split big test cases

--- examples/unit-test-framework-test.sh
+++ examples/unit-test-framework-test.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 export CFLIB_INC_PATH="$HOME/src/common-functions-lib"; source "$CFLIB_INC_PATH/cflib-import.sh"
 require colorize
-
-#export BESTING_DEBUG=true
 require mockframework
+
+#declare -a BESTING_ASSERT_DIFF_OPTS=(-w -y)
 
 @setup {
     ignore-command echo
@@ -24,28 +24,26 @@ test1() {
     @mock ls file1 file2 -- file2 file1
     ls $(find . -name fn) | xargs -n1 -- do-something
 
-
-    project_path=/src/project
-    cd "${project_path%/*}"
-    sudo rm -rf $project_path
-
-
-    @mock git rev-parse --abbrev-ref --symbolic-full-name '@{u}' -- return 1
-    gp
-
-    err error 2>&1
-    /bin/ls /foo 2>&1
 }
 test1-assert() {
     ls file1 file2
 
     do-something file2
     do-something file1
+}
 
+test2() {
+    project_path=/src/project
+    cd "${project_path%/*}"
+    sudo rm -rf $project_path
+
+    err error 2>&1
+    /bin/ls /foo 2>&1
+}
+test2-assert() {
     cd /src
     sudo rm -rf /src/project
 
-    git push -u remote-master branch-name
     printf "\e[1;31merror\e[0;m\n"
     out "ls: /foo: No such file or directory"
 }
@@ -61,6 +59,13 @@ test-gp-1-assert() {
     git push remote-master branch-name
 }
 
+test-gp-2() {
+    @mock git rev-parse --abbrev-ref --symbolic-full-name '@{u}' -- return 1
+    gp
+}
+test-gp-2-assert() {
+    git push -u remote-master branch-name
+}
 
 test-besting-real-command-mock-builtin() {
     @mock command which grep -- stdout fake-grep
